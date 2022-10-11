6901728622133c45e18afe59362200212701c987
kdaic <develop@kdaic.net>
2022-10-11 19:41:22 +0900

    add config.ini & scripts/mi-lib-clone.bat for windows

---
+++ scripts/mi-lib-clone.bat
@@ -0,0 +1,78 @@
+@echo off
+
+rem cloning required packages...
+
+set CONFIG=config.ini
+echo CONFIG = %CONFIG%
+
+set SCRIPTS_DIR=%~dp0
+echo SCRIPTS_DIR=%SCRIPTS_DIR%
+
+set CURDIR=%CD%
+echo CURDIR=%CURDIR%
+
+
+if not exist %CONFIG% (
+    echo Not exists '%CONFIG%' in current direcotry : %CURDIR% .
+    echo Try 'scripts\mi-lib-clone.bat'
+    exit /b 1
+)
+
+
+@REM parse CONFIG
+for /f "tokens=1,* delims==" %%a in ( %CONFIG% ) do (
+    set %%a=%%b
+)
+echo PREFIX = %PREFIX%
+echo LIBS   = %LIBS%
+
+
+@REM get LIB source
+for %%d in ( %LIBS% ) do (
+    echo LIB = %%d
+
+    if not exist %%d (
+        echo Not exists %%d directory.
+        call :git_clone %%d
+    ) else (
+        echo %%d directory already exists.
+        call :download_and_unzip %%d
+    )
+)
+
+
+
+exit /b 0
+@REM End of Main Routine
+
+@REM --- subroutine -----------------------------------
+
+:git_clone
+    echo git_clone %LIB%
+    set LIB=%1
+    
+    echo git.exe clone https://github.com/mi-lib/%LIB%
+    echo git.exe clone https://github.com/mi-lib/%LIB%
+    
+    exit /b 0
+
+
+:download_and_unzip
+    echo download_and_unzip %LIB%
+    set LIB=%1
+    set ZIP_FILENAME=%LIB%-main
+    
+    echo bitsadmin.exe /TRANSFER httpsdowload /download /priority foreground https://github.com/mi-lib/%LIB%/archive/refs/heads/main.zip %CURDIR%\%ZIP_FILENAME%.zip
+    bitsadmin.exe /TRANSFER httpsdowload /download /priority foreground https://github.com/mi-lib/%LIB%/archive/refs/heads/main.zip %CURDIR%\%ZIP_FILENAME%.zip
+    
+    echo call powershell -command "Expand-Archive -Force %ZIP_FILENAME%.zip %CURDIR%"
+    call powershell -command "Expand-Archive -Force %ZIP_FILENAME%.zip %CURDIR%"
+    
+    echo xcopy /Y/S/I %ZIP_FILENAME%\* %LIB%
+    xcopy /Y/S/I %ZIP_FILENAME%\* %LIB%
+    
+    echo rmdir /s/q %ZIP_FILENAME%
+    rmdir /s/q %ZIP_FILENAME%
+    
+    exit /b 0
+
